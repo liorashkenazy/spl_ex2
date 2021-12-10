e9329a200aecb19424719897f8b8533410a543aa
liorashkenazy <la26597@gmail.com>
2021-12-10 18:07:17 +0200

    implementing ConferenceInformation, ConferenceService and creating TickBroadcast, PublishConferenceBroadcast, PublishResultsEvent

--- src/main/java/bgu/spl/mics/application/CRMSRunner.java
+++ src/main/java/bgu/spl/mics/application/CRMSRunner.java
@@ -5,10 +5,8 @@ package bgu.spl.mics.application;
  * In the end, you should output a text file.
  */
 public class CRMSRunner {
-    enum Lior {la, ashkenal, lir };
-
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
 
     }
 }
