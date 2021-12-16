bef286e8de85c3717bd99da67c67a5c8726f147f
liorashkenazy <la26597@gmail.com>
2021-12-16 13:04:20 +0200

    gpu service and cpu service - not final

--- src/main/java/bgu/spl/mics/application/services/GPUService.java
+++ src/main/java/bgu/spl/mics/application/services/GPUService.java
@@ -1,11 +1,17 @@
 package bgu.spl.mics.application.services;
 
+import bgu.spl.mics.Callback;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.TestModelEvent;
+import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.messages.TrainModelEvent;
+import bgu.spl.mics.application.objects.Cluster;
+import bgu.spl.mics.application.objects.GPU;
 
 /**
  * GPU service is responsible for handling the
  * {@link TrainModelEvent} and {@link TestModelEvent},
- * in addition to sending the {@link DataPreProcessEvent}.
+
  * This class may not hold references for objects which it is not responsible for.
  *
  * You can add private fields and public methods to this class.
@@ -13,14 +19,41 @@ import bgu.spl.mics.MicroService;
  */
 public class GPUService extends MicroService {
 
-    public GPUService(String name) {
-        super("Change_This_Name");
-        // TODO Implement this
+    private GPU gpu;
+
+    public GPUService(String name, String type, Cluster cluster) {
+        super(name);
+        gpu = new GPU(type);
     }
 
     @Override
     protected void initialize() {
-        // TODO Implement this
+        subscribeEvent(TrainModelEvent.class,new TrainModelCallback());
+        subscribeEvent(TestModelEvent.class,new TestModelCallback());
+        subscribeBroadcast(TickBroadcast.class,new TickCallback());
+    }
 
+    private class TickCallback implements Callback<TickBroadcast> {
+        /**
+         * This function should be called every time a tick occurs.
+         * <p>
+         * @param tickBroadcast the message that was taken from the message queue.
+         */
+        public void call(TickBroadcast tickBroadcast) {
+            gpu.tick();
+        }
+    }
+
+    private class TrainModelCallback implements Callback<TrainModelEvent> {
+        public void call(TrainModelEvent trainModelEvent) {
+            //TODO
+            gpu.trainModel(trainModelEvent.getModel(), );
+        }
+    }
+
+    private class TestModelCallback implements Callback<TestModelEvent> {
+        public void call(TestModelEvent testModelEvent) {
+            gpu.testModel(testModelEvent.getModel());
+        }
     }
 }
