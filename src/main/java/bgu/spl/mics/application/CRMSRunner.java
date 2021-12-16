d6cf9df23b639bd3d525cdd2de45b026e49e4c48
liorashkenazy <la26597@gmail.com>
2021-12-16 12:08:39 +0200

    json parsing - delete unneeded prints and spaced

--- src/main/java/bgu/spl/mics/application/CRMSRunner.java
+++ src/main/java/bgu/spl/mics/application/CRMSRunner.java
@@ -17,7 +17,6 @@ import java.io.FileReader;
  */
 public class CRMSRunner {
     public static void main(String[] args) {
-
         parsingInput();
         System.out.println("Hello World!");
     }
@@ -48,7 +47,6 @@ public class CRMSRunner {
             i = 0;
             for (JsonElement cpu : cpus_json_array) {
                 cpus_array[i] = new CPU(cpu.getAsInt());
-                System.out.println(cpus_array[i].toString());
                 i++;
             }
 
@@ -62,59 +60,6 @@ public class CRMSRunner {
             // Extracting Duration
             int duration = fileObject.get("Duration").getAsInt();
 
-//            JsonArray cpu_json_array = fileObject.get("CPUS").getAsJsonArray();
-//            System.out.println(cpu_json_array.toString());
-
         } catch (Exception e){System.out.println(e);}
-
     }
-
-//
-//    public static void parsingInput() {
-//        try {
-//            File input_file = new File("C:/Users/la265/OneDrive/Desktop/Uni/ComputerScience/SPL/ex2/example_input.json");
-//            JsonElement fileElement = JsonParser.parseReader(new FileReader(input_file));
-//            JsonObject fileObject = fileElement.getAsJsonObject();
-//
-//
-//            Integer tickTime = fileObject.get("TickTime").getAsInt();
-//            Integer duration = fileObject.get("Duration").getAsInt();
-//            System.out.println("TickTime = " + tickTime);
-//            System.out.println("Duration = " + duration);
-//
-//
-//            JsonArray jsonArrayOfStudents = fileObject.get("Students").getAsJsonArray();
-//            for (JsonElement student : jsonArrayOfStudents) {
-//                JsonObject studentJsonObject = student.getAsJsonObject();
-//                String student_name = studentJsonObject.get("name").getAsString();
-//                int name = studentJsonObject.;
-//                String department;
-//                String status;
-//                Model[] modelsArray;
-//            }
-
-//            File input_file_path = new File("C:/Users/la265/OneDrive/Desktop/Uni/ComputerScience/SPL/ex2/example_input.json");
-//            Reader reader = new FileReader(input_file_path);
-//            Gson gson = new Gson();
-//            JsonArray json=(JsonArray)gson.fromJson(reader, JsonElement.class);
-//            for(int iterator=0;iterator<json.size();iterator++) {
-//                JsonElement oElement=json.get(iterator);
-//                Student[] st =gson.fromJson(reader,Student[].class);
-//            }
-////
-////                System.out.println("s");
-//            Gson gson = new Gson();
-//            File input_file = new File("C:/Users/la265/OneDrive/Desktop/Uni/ComputerScience/SPL/ex2/example_input.json");
-//            JsonElement fileElement = JsonParser.parseReader(new FileReader(input_file));
-//            JsonObject fileObject = fileElement.getAsJsonObject();
-//            JsonArray student_json_array = fileObject.get("Student").getAsJsonArray();
-//            String json = "[{ 'name': 'lior','department':
-//            private String department;
-//            private Student.Degree status;}
-//            Student[] studentArray = gson.fromJson(student_json_array,Student[].class);
-//            System.out.println(studentArray.length);
-//
-//        } catch (Exception ex) {
-//        }
-//    }
 }
